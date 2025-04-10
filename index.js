46d36db52aab4d65a3a1e9fd1482b6d86927d36c
Kubit <kubit.lab.dev@gmail.com>
2025-04-10 10:11:09 +0200

    Add additionalPlugins parameter

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = ({
   browserList = ['> 1%', 'last 2 versions', 'not ie <= 11'],
   globals,
   isReact = true,
+  additionalPlugins = {},
 }) => {
   return defineConfig(
     eslint.configs.recommended,
@@ -59,6 +60,7 @@ module.exports = ({
           'no-relative-import-paths': noRelativeImportPathsEslint,
           compat: compatEslint,
           perfectionist,
+          ...(additionalPlugins || {}),
         },
         settings: {
           ...(isReact && {
@@ -107,74 +109,78 @@ module.exports = ({
           ], // Disallow magic numbers to enforce the use of named constants
 
           ...(isReact && {
-          // React rules
-          'react/react-in-jsx-scope': 'off', // React 17+ does not require React to be in scope
-          'react/self-closing-comp': 'error', // Enforce self-closing tags for components without children
-          'react/jsx-boolean-value': ['error', 'always'], // Enforce boolean attributes notation in JSX
-          'react/jsx-fragments': ['error', 'syntax'], // Enforce shorthand or standard form for React fragments
-          'react/jsx-no-useless-fragment': 'error', // Disallow unnecessary fragments
-          'react/jsx-pascal-case': 'error', // Enforce PascalCase for user-defined JSX components
-          'react/no-array-index-key': 'error', // Prevent usage of array index in keys
-          'react/no-danger': 'error', // Prevent usage of dangerous JSX properties
-          'react/no-multi-comp': 'error', // Prevent multiple component definitions per file
-          'react/no-unused-state': 'error', // Prevent state fields from being unused
-          'react/no-unstable-nested-components': 'error', // Prevent creating unstable components inside components
-          'react/sort-comp': 'error', // Enforce component methods order
-          'react/jsx-no-duplicate-props': 'error', // Prevent duplicate props in JSX
-          'react/jsx-no-target-blank': 'error', // Prevent usage of unsafe target='_blank'
-          'react/jsx-key': 'error', // Enforce key prop for elements in arrays or iterators
-          'react/jsx-no-comment-textnodes': 'error', // Prevent comments from being inserted as text nodes
-          'react/jsx-no-undef': 'error', // Disallow undeclared variables in JSX
-          'react/jsx-uses-react': 'error', // Prevent React to be incorrectly marked as unused
-          'react/jsx-uses-vars': 'error', // Prevent variables used in JSX to be incorrectly marked as unused
-          'react/jsx-wrap-multilines': 'error', // Enforce line breaks in JSX
-          'react/jsx-indent': ['error', 2], // Enforce consistent indentation in JSX
-          'react/jsx-indent-props': ['error', 2], // Enforce consistent indentation for props in JSX
-          'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }], // Enforce spacing around the closing bracket of JSX tags
-          'react/jsx-equals-spacing': ['error', 'never'], // Disallow spaces around equal signs in JSX attributes
-          'react/jsx-closing-bracket-location': ['error', 'line-aligned'], // Enforce the location of the closing bracket for multiline JSX elements
-          'react/jsx-closing-tag-location': 'error', // Enforce the location of the closing tag for JSX elements
-          'react/jsx-first-prop-new-line': ['error', 'multiline'], // Enforce the location of the first prop in JSX
-          'react/jsx-max-props-per-line': ['error', { when: 'multiline' }], // Limit the number of props per line in JSX
-          'react/jsx-props-no-multi-spaces': 'error', // Disallow multiple spaces between inline JSX props
-          'react/jsx-sort-props': [
-            'error',
-            { callbacksLast: true, shorthandFirst: true, reservedFirst: true },
-          ], // Enforce props alphabetical sorting
-          'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }], // Enforce space before self-closing bracket in JSX
-          'jsx-quotes': ['error', 'prefer-double'], // Enforce the consistent use of double quotes in JSX attributes
-          'jsx-a11y/alt-text': 'error', // Enforce alt text on images
-          'jsx-a11y/anchor-has-content': 'error', // Enforce anchor elements to contain accessible content
-          'jsx-a11y/anchor-is-valid': 'error', // Enforce valid anchor elements
-          'jsx-a11y/aria-activedescendant-has-tabindex': 'error', // Enforce elements with aria-activedescendant are tabbable
-          'jsx-a11y/aria-props': 'error', // Enforce all aria-* props are valid
-          'jsx-a11y/aria-proptypes': 'error', // Enforce ARIA state and property values are valid
-          'jsx-a11y/aria-role': 'error', // Enforce that elements with ARIA roles must use a valid, non-abstract ARIA role
-          'jsx-a11y/aria-unsupported-elements': 'error', // Enforce that elements that do not support ARIA roles, states, and properties do not have those attributes
-          'jsx-a11y/click-events-have-key-events': 'error', // Enforce a clickable non-interactive element has at least one keyboard event listener
-          'jsx-a11y/heading-has-content': 'error', // Enforce heading elements have content and are not empty
-          'jsx-a11y/html-has-lang': 'error', // Enforce <html> element has a lang attribute
-          'jsx-a11y/iframe-has-title': 'error', // Enforce iframe elements have a title attribute
-          'jsx-a11y/img-redundant-alt': 'error', // Enforce <img> alt prop does not contain the word "image", "picture", or "photo"
-          'jsx-a11y/interactive-supports-focus': 'error', // Enforce that elements with interactive handlers like onClick must be focusable
-          'jsx-a11y/label-has-associated-control': 'error', // Enforce that <label> elements have the htmlFor attribute
-          'jsx-a11y/lang': 'error', // Enforce lang attribute has a valid value
-          'jsx-a11y/media-has-caption': 'error', // Enforce that media elements like <audio> and <video> must have a <track> for captions
-          'jsx-a11y/mouse-events-have-key-events': 'error', // Enforce that onMouseOver/onMouseOut are accompanied by onFocus/onBlur for keyboard-only users
-          'jsx-a11y/no-access-key': 'error', // Enforce that the accessKey prop is not used on any element
-          'jsx-a11y/no-autofocus': 'error', // Enforce that autoFocus prop is not used on elements
-          'jsx-a11y/no-distracting-elements': 'error', // Enforce that distracting elements are not used
-          'jsx-a11y/no-interactive-element-to-noninteractive-role': 'warn', // Enforce that interactive elements are not assigned non-interactive roles
-          'jsx-a11y/no-noninteractive-element-interactions': 'error', // Enforce that non-interactive, visible elements (such as <div>) that have click handlers use role attribute
-          'jsx-a11y/no-noninteractive-element-to-interactive-role': 'error', // Enforce that non-interactive elements are not assigned interactive roles
-          'jsx-a11y/no-noninteractive-tabindex': 'error', // Enforce that tabIndex is not assigned to non-interactive elements
-          'jsx-a11y/no-redundant-roles': 'error', // Enforce that redundant roles are not used
-          'jsx-a11y/no-static-element-interactions': 'error', // Enforce that static elements (such as <div>) that have click handlers use role attribute
-          'jsx-a11y/role-has-required-aria-props': 'error', // Enforce that elements with ARIA roles must have all required attributes for that role
-          'jsx-a11y/role-supports-aria-props': 'error', // Enforce that elements with explicit or implicit roles defined contain only aria-* properties supported by that role
-          'jsx-a11y/scope': 'error', // Enforce that <th> elements have a scope attribute
-          'jsx-a11y/tabindex-no-positive': 'error', // Enforce that tabIndex value is not greater than zero
-        }),
+            // React rules
+            'react/react-in-jsx-scope': 'off', // React 17+ does not require React to be in scope
+            'react/self-closing-comp': 'error', // Enforce self-closing tags for components without children
+            'react/jsx-boolean-value': ['error', 'always'], // Enforce boolean attributes notation in JSX
+            'react/jsx-fragments': ['error', 'syntax'], // Enforce shorthand or standard form for React fragments
+            'react/jsx-no-useless-fragment': 'error', // Disallow unnecessary fragments
+            'react/jsx-pascal-case': 'error', // Enforce PascalCase for user-defined JSX components
+            'react/no-array-index-key': 'error', // Prevent usage of array index in keys
+            'react/no-danger': 'error', // Prevent usage of dangerous JSX properties
+            'react/no-multi-comp': 'error', // Prevent multiple component definitions per file
+            'react/no-unused-state': 'error', // Prevent state fields from being unused
+            'react/no-unstable-nested-components': 'error', // Prevent creating unstable components inside components
+            'react/sort-comp': 'error', // Enforce component methods order
+            'react/jsx-no-duplicate-props': 'error', // Prevent duplicate props in JSX
+            'react/jsx-no-target-blank': 'error', // Prevent usage of unsafe target='_blank'
+            'react/jsx-key': 'error', // Enforce key prop for elements in arrays or iterators
+            'react/jsx-no-comment-textnodes': 'error', // Prevent comments from being inserted as text nodes
+            'react/jsx-no-undef': 'error', // Disallow undeclared variables in JSX
+            'react/jsx-uses-react': 'error', // Prevent React to be incorrectly marked as unused
+            'react/jsx-uses-vars': 'error', // Prevent variables used in JSX to be incorrectly marked as unused
+            'react/jsx-wrap-multilines': 'error', // Enforce line breaks in JSX
+            'react/jsx-indent': ['error', 2], // Enforce consistent indentation in JSX
+            'react/jsx-indent-props': ['error', 2], // Enforce consistent indentation for props in JSX
+            'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }], // Enforce spacing around the closing bracket of JSX tags
+            'react/jsx-equals-spacing': ['error', 'never'], // Disallow spaces around equal signs in JSX attributes
+            'react/jsx-closing-bracket-location': ['error', 'line-aligned'], // Enforce the location of the closing bracket for multiline JSX elements
+            'react/jsx-closing-tag-location': 'error', // Enforce the location of the closing tag for JSX elements
+            'react/jsx-first-prop-new-line': ['error', 'multiline'], // Enforce the location of the first prop in JSX
+            'react/jsx-max-props-per-line': ['error', { when: 'multiline' }], // Limit the number of props per line in JSX
+            'react/jsx-props-no-multi-spaces': 'error', // Disallow multiple spaces between inline JSX props
+            'react/jsx-sort-props': [
+              'error',
+              {
+                callbacksLast: true,
+                shorthandFirst: true,
+                reservedFirst: true,
+              },
+            ], // Enforce props alphabetical sorting
+            'react/jsx-tag-spacing': ['error', { beforeSelfClosing: 'always' }], // Enforce space before self-closing bracket in JSX
+            'jsx-quotes': ['error', 'prefer-double'], // Enforce the consistent use of double quotes in JSX attributes
+            'jsx-a11y/alt-text': 'error', // Enforce alt text on images
+            'jsx-a11y/anchor-has-content': 'error', // Enforce anchor elements to contain accessible content
+            'jsx-a11y/anchor-is-valid': 'error', // Enforce valid anchor elements
+            'jsx-a11y/aria-activedescendant-has-tabindex': 'error', // Enforce elements with aria-activedescendant are tabbable
+            'jsx-a11y/aria-props': 'error', // Enforce all aria-* props are valid
+            'jsx-a11y/aria-proptypes': 'error', // Enforce ARIA state and property values are valid
+            'jsx-a11y/aria-role': 'error', // Enforce that elements with ARIA roles must use a valid, non-abstract ARIA role
+            'jsx-a11y/aria-unsupported-elements': 'error', // Enforce that elements that do not support ARIA roles, states, and properties do not have those attributes
+            'jsx-a11y/click-events-have-key-events': 'error', // Enforce a clickable non-interactive element has at least one keyboard event listener
+            'jsx-a11y/heading-has-content': 'error', // Enforce heading elements have content and are not empty
+            'jsx-a11y/html-has-lang': 'error', // Enforce <html> element has a lang attribute
+            'jsx-a11y/iframe-has-title': 'error', // Enforce iframe elements have a title attribute
+            'jsx-a11y/img-redundant-alt': 'error', // Enforce <img> alt prop does not contain the word "image", "picture", or "photo"
+            'jsx-a11y/interactive-supports-focus': 'error', // Enforce that elements with interactive handlers like onClick must be focusable
+            'jsx-a11y/label-has-associated-control': 'error', // Enforce that <label> elements have the htmlFor attribute
+            'jsx-a11y/lang': 'error', // Enforce lang attribute has a valid value
+            'jsx-a11y/media-has-caption': 'error', // Enforce that media elements like <audio> and <video> must have a <track> for captions
+            'jsx-a11y/mouse-events-have-key-events': 'error', // Enforce that onMouseOver/onMouseOut are accompanied by onFocus/onBlur for keyboard-only users
+            'jsx-a11y/no-access-key': 'error', // Enforce that the accessKey prop is not used on any element
+            'jsx-a11y/no-autofocus': 'error', // Enforce that autoFocus prop is not used on elements
+            'jsx-a11y/no-distracting-elements': 'error', // Enforce that distracting elements are not used
+            'jsx-a11y/no-interactive-element-to-noninteractive-role': 'warn', // Enforce that interactive elements are not assigned non-interactive roles
+            'jsx-a11y/no-noninteractive-element-interactions': 'error', // Enforce that non-interactive, visible elements (such as <div>) that have click handlers use role attribute
+            'jsx-a11y/no-noninteractive-element-to-interactive-role': 'error', // Enforce that non-interactive elements are not assigned interactive roles
+            'jsx-a11y/no-noninteractive-tabindex': 'error', // Enforce that tabIndex is not assigned to non-interactive elements
+            'jsx-a11y/no-redundant-roles': 'error', // Enforce that redundant roles are not used
+            'jsx-a11y/no-static-element-interactions': 'error', // Enforce that static elements (such as <div>) that have click handlers use role attribute
+            'jsx-a11y/role-has-required-aria-props': 'error', // Enforce that elements with ARIA roles must have all required attributes for that role
+            'jsx-a11y/role-supports-aria-props': 'error', // Enforce that elements with explicit or implicit roles defined contain only aria-* properties supported by that role
+            'jsx-a11y/scope': 'error', // Enforce that <th> elements have a scope attribute
+            'jsx-a11y/tabindex-no-positive': 'error', // Enforce that tabIndex value is not greater than zero
+          }),
 
           // Prettier rule
           'prettier/prettier': ['error'], // Enforce Prettier formatting
